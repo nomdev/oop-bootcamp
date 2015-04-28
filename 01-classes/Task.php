aad2b4271966ec17b623529d96a1752cb17b38a7
sims <sn@codeport.gr>
2015-04-29 01:15:51 +0300

    oop-bootcamp

--- 01-classes/Task.php
+++ 01-classes/Task.php
@@ -2,15 +2,23 @@
 
 class Task {
     public $description;
+    public $completed = false;
 
     public function __construct($description){
         $this->description = $description;
     }
+
+
+    public function complete(){
+        $this->completed = true;
+    }
+
 }
 
-$task = new Task('alo');
-$task2 = new Task('alo2');
+    $task = new Task('alo');
+        var_dump($task);
+    $task->complete();
+        var_dump($task->completed);
 
-
-var_dump($task);
-var_dump($task2);
+    $task2 = new Task('alo2');
+    var_dump($task2);
