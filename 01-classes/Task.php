5907349df186a1b039126c119ed273d03750945a
sims <sn@codeport.gr>
2015-04-29 01:06:13 +0300

    oop-bootcamp

--- 01-classes/Task.php
+++ 01-classes/Task.php
@@ -4,6 +4,13 @@ class Task {
     public $description;
 
     public function __construct($description){
-        var_dump($description);
+        $this->description = $description;
     }
 }
+
+$task = new Task('alo');
+$task2 = new Task('alo2');
+
+
+var_dump($task);
+var_dump($task2);
