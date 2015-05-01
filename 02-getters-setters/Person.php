74e71aa154a01c093858e4d0abacb44096f27591
sims <sn@codeport.gr>
2015-05-01 23:46:23 +0300

    oop-bootcamp

---
+++ 02-getters-setters/Person.php
@@ -0,0 +1,18 @@
+<?php
+class Person{
+    public $name;
+    public $age;
+
+    public function __construct($name){
+        $this->$name = $name;
+    }
+    public function setAge($age){
+        if($age < 18){
+            throw new Exception("person is not old enough");
+        }
+        $this->age = $age;
+    }
+}
+
+$john = new Person('John Doe');
+$john->setAge(30);
