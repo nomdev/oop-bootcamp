eff6c9b08ab04f2b12635fa5519638bdbb094dd6
sims <sn@codeport.gr>
2015-05-02 00:39:14 +0300

    oop-bootcamp

--- 03-encapsulation/LightSwitch.php
+++ 03-encapsulation/LightSwitch.php
@@ -2,4 +2,24 @@
 
 class LightSwitch {
 
+    public function on(){
+
+    }
+    public function off(){
+
+    }
+
+    //private function: outside of this class nobody have access to this method!!
+    private function connect(){
+
+    }
+    private function activate(){
+
+    }
+
 }
+
+$lightswitch = new LightSwitch();
+
+var_dump($lightswitch->connect());
+//PHP fatal error. Call to private method LightSwitch::connect() from context.
